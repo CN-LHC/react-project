8cba3a31864725a58df238ba3802dbc8505f1730
liuhanchuan <hanchuan.liu@superdata-tech.com>
2021-03-08 11:39:07 +0800

    使用react-router-dom搭建hash路由

--- src/app.js
+++ src/app.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-// import { browserHistory, BrowserRouter as Router, Route, IndexRoute } from 'react-router';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import Layout from './layouts/index';
 import Demo from './routes/demo/index';
 
 export default class App extends Component {
   render() {
     return (
-      <Layout>
-        <Demo />
-      </Layout>
-      // <Router history={browserHistory}>
-      //   <Route path='/' component={Layout}>
-      //     <IndexRoute component={Demo} />
-      //   </Route>
-      // </Router>
+      <HashRouter>
+        <Layout>
+          <Switch>
+              <Route exact path="/demo" component={Demo}/>
+          </Switch>
+        </Layout>
+      </HashRouter>
     );
   }
 }
